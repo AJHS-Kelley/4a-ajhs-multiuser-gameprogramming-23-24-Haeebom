5302c76dc3e6eb6573522da0bd88dd5ea20bfdb9
Naomi Williams <90346561+Haeebom@users.noreply.github.com>
2023-09-27 12:36:05 -0400

    commited

---
+++ 01_operators/programTemple.cs
@@ -0,0 +1,85 @@
+// Naomi Williams, Hello World, v0.2
+using System;
+
+namespace operators
+{
+   class program
+   {
+     static void Main(string[] args)  
+     {
+      // Creat TWO sting, Interger, and Float variables.
+      string myString1 = "I like potao salad."  ;
+      string myString2 = "Sometimes i like to run in the rain." ;
+
+      int myInt1 = 9001;
+      int myInt2 = -5;
+
+      float myFloat1 = 0.0F;
+      float myFloat1 = -1.5F;
+
+      // Arithmetic Operators
+      Console.WriteLine(myInt1 + myInt2);
+      Console.WriteLine(myString1 + myString2);
+      Console.WriteLine(myFloat1 + myFloat2);
+      Console.WriteLine(myInt1 + myFloat2);
+
+      Console.WriteLine(myInt1 - myInt2);
+//      Console.WriteLine(myString1 - myString2);
+      Console.WriteLine(myFloat1 - myFloat2);
+      Console.WriteLine(myInt1 - myFloat2);
+
+      Console.WriteLine(myInt1 * myInt2);
+//     Console.WriteLine(myString1 * myString2);
+      Console.WriteLine(myFloat1 * myFloat2);
+      Console.WriteLine(myInt1 * myFloat2);
+
+      Console.WriteLine(myInt1 / myInt2);
+//      Console.WriteLine(myString1 / myString2);
+      Console.WriteLine(myFloat1 / myFloat2);
+      Console.WriteLine(myInt1 / myFloat2);
+
+      // Modulus
+      Console.WriteLine("Modulus");
+      Console.WriteLine(10 % 2);
+      Console.WriteLine(9 % 4);
+
+      // Increment
+      int myInt3 = 1;
+      myInt2++;
+      Console.WriteLine(myInt3);
+
+      // Decrement
+      int myInt4 = 2;
+      myInt3--;
+      Console.WriteLine(myInt4);
+
+      // Assignment Operators
+      //
+      //
+      //
+      //
+      //
+
+      // Comparison
+      //
+      //
+      //
+      //
+      //
+      //
+
+      // Logical Operators
+      //
+      //
+      //
+      
+
+
+     
+
+
+     }
+   }  
+}
+
+    
---
+++ numberGuest.cs
@@ -0,0 +1,39 @@
+// Naomi Williams, Program template, v0.01
+/*
+generate secret number from a defined range of numbers(i.e. 0-1,0-50,0-100)
+Print game instructions including range and num. of guesses allowed.
+	MATCH == first player toscore 3 points.
+	ROUND == guessing a specific number, until correct or no more attempts.
+Ask the player what difficulty they want to play on.
+ask the player what their guess is.
+determine if guess is correct or not.
+    if guess is correct{ 
+	tell them they guessed correctly
+	award the player the point.
+	} else {
+		tell them they are wrong.
+		
+		check to see if they have guesses remaining{
+			if guess remain:
+			allow player to guess again.
+		    } else {
+				cpu wins the round
+
+	
+	
+	}
+	
+	
+*/
+using System;
+
+namespace numberGuess
+{
+	class numberGuess
+	{
+		static void Main (string [] args)
+		{
+			
+		}
+	}
+}
