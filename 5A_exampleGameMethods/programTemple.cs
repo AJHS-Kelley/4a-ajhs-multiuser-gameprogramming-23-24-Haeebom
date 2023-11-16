1b7902d17379598231c3e580c76659f3fed3fb10
Naomi Williams <90346561+Haeebom@users.noreply.github.com>
2023-11-16 13:44:15 -0500

    Rock Paper Sciccors

--- 5A_exampleGameMethods/programTemple.cs
+++ 5A_exampleGameMethods/programTemple.cs
@@ -1,19 +1,24 @@
-// Naomi Williams, Hello World, v0.2
+// Naomi Williams, Example Game Methods, v0.1
 using System;
 
-namespace operators
+namespace rockPaperScissors
 {
-   class program
-   {
+    class Player
+    {
+        public string playername;
+        private int playerscore;
 
-      
-
-
-     
-
-
-     }
-   }  
+        public int Score
+        {
+            get { return playerscore; }
+            set { playerscore = value; }
+        }
+    }
+    class RockPaperScissors
+    {
+        public static string p_rps;
+        private static int c_rps;
+        private static int computerscore;
+    }
 }
-
     
