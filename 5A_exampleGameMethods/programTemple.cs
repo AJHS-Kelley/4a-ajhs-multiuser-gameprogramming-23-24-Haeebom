62bc7a6d5cb849065169578e3559608091a1da31
Naomi Williams <90346561+Haeebom@users.noreply.github.com>
2023-11-20 13:41:48 -0500

    Finale code.
    Finished code.

--- 5A_exampleGameMethods/programTemple.cs
+++ 5A_exampleGameMethods/programTemple.cs
@@ -1,7 +1,7 @@
 // Naomi Williams, Example Game Methods, v0.1
 using System;
 
-namespace rockPaperScissors
+namespace rockpaperscissors
 {
     class Player
     {
@@ -19,8 +19,8 @@ namespace rockPaperScissors
         public static string p_rps;
         private static int c_rps;
         private static int computerscore;
-    }
-   public static void Initialize(Player player)
+
+        public static void Initialize(Player player)
         {
             player.Score = 0;
             computerscore = 0;          
@@ -29,8 +29,9 @@ namespace rockPaperScissors
         public static void Board(Player player)
         {
             Console.WriteLine("Rock, Paper, Sciccors ... SHOOT!\n", player.playername, player.Score, computerscore);
-        } 
-          public static int ComputerRPS()
+        }
+
+        public static int ComputerRPS()
         {
             Random c = new Random();
             c_rps = c.Next(1, 4);
@@ -41,7 +42,8 @@ namespace rockPaperScissors
         public static void Check(int c, Player player)
         {
             c = ComputerRPS();
-             switch(c)
+
+            switch(c)
             {
                 case 1:
 
@@ -56,7 +58,7 @@ namespace rockPaperScissors
                     }
                     else if (p_rps == "S")
                     {
-                      Console.WriteLine("Computer chose rock.\nRock beats scissors.Computer wins this round.");
+                        Console.WriteLine("Computer chose rock.\nRock beats scissors. Computer wins this round.");
                         computerscore++;
                     }
 
@@ -99,29 +101,90 @@ namespace rockPaperScissors
                     }
 
                     break;
+            }
+        }
 
-                    public static bool WhoWins(Player player)
-                 {
-                  if (player.Score == 5)
-                 {
-                   Console.WriteLine("\n{0} wins the game.\n",player.playername);
-                   return true;
-                 }
-                 if (computerscore == 5)
-                 {
-                   Console.WriteLine("\nComputer wins the game.\n");
-                   return true;
-                  }
+        public static bool WhoWins(Player player)
+        {
+            if (player.Score == 5)
+            {
+                Console.WriteLine("\n{0} wins the game.\n",player.playername);
+                return true;
+            }
+            if (computerscore == 5)
+            {
+                Console.WriteLine("\nComputer wins the game.\n");
+                return true;
+            }
 
-                   return false;
-                 }
-              }
+            return false;
+        }
+    }
+    class Program
+    {
+        public static bool play;
+        public static string startgame;
 
-                
-                 
-            }  
-}   
+        static void StartGameOrNot()
+        {
+            do
+            {                
+                startgame = Console.ReadLine().ToUpper();
+                startgame.ToUpper();
 
-                   
-         
-     
+                if (startgame == "Y")
+                {
+                    play = true;
+                }
+                else if (startgame == "N")
+                {
+                    Console.WriteLine("\nOkay then, goodbye");
+                    Environment.Exit(0);
+                }
+                else
+                {
+                    Console.Write("\nInvalid. Do you want to start the game? [Y/N] --> ");
+                }
+            } while (startgame != "Y" && startgame != "N");
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Do you want to start the game? [Y/N] --> ");
+            StartGameOrNot();
+
+            Player player1 = new Player();
+
+            Console.Clear();
+            Console.Write("\n\n\tWhat is your name? --> ");
+            player1.playername = Console.ReadLine();
+
+            Console.Clear();
+            RockPaperScissors.Initialize(player1);
+
+            while (play)
+            {                              
+                RockPaperScissors.Board(player1);
+
+                do
+                {
+                    Console.Write("Rock, paper, scissors? [R/P/S] --> ");
+                    RockPaperScissors.p_rps = Console.ReadLine().ToUpper();                  
+                } while (RockPaperScissors.p_rps == "R" && RockPaperScissors.p_rps == "P" && RockPaperScissors.p_rps == "S");
+
+                int c = RockPaperScissors.ComputerRPS();
+
+                Console.Clear();
+
+                RockPaperScissors.Check(c, player1);
+
+                if(RockPaperScissors.WhoWins(player1))
+                {
+                    Console.Write("Replay? --> ");
+                    StartGameOrNot();
+                    RockPaperScissors.Initialize(player1);
+                    Console.Clear();
+                }
+            }
+        }
+    }
+}
