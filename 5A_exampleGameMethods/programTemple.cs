5e5b2acfd6dd596a8a6b5092d3aa417f033cd6c7
Naomi Williams <90346561+Haeebom@users.noreply.github.com>
2023-12-13 13:05:18 -0500

    Update programTemple.cs

--- 5A_exampleGameMethods/programTemple.cs
+++ 5A_exampleGameMethods/programTemple.cs
@@ -6,95 +6,95 @@ using System;
 // collect users response in a variable
 // string.StartsWith("x") will check to see if a string starts with that letter, returns true if it does. 
 
-namespace languagepicker
+namespace language
 /*
 No for loop. 
 No while loop. 
 Missing parameters for methods. 
 */
 
+{
+class person 
+  {
+   static string difficulty = "Empty";
+   static string language();
+   Console.WriteLine("Would you like to learn a language?\n");
+   string answer = Console.ReadLine().ToLower();
+   if (answer == "yes") answer.ToLower().StartsWith("y")
     {
-      class person 
-       {
-         static string difficulty = "Empty";
-         static string language();
-         Console.WriteLine("Would you like to learn a language?\n");
-         string answer = Console.ReadLine().ToLower();
-         if (answer == "yes") answer.ToLower().StartsWith("y")
-         {
-            Console.WriteLine(" Would you like to learn Spanish, Russian, Japanese, or Chinese\n");
-            answer = Console.ReadLine().ToLower();
-            if (answer == "spanish")answer.ToLower().StartsWith("s")
-          
-         {
-            { 
-              Console.WriteLine(" You've selected Spanish!\n");
-              difficulty = "Easy"; 
-            } else if (answer == "japanese") 
-            {
-             Console.WriteLine("You've selected Japanese \n"); 
-             difficulty = "Medium";
-            }else if (answer == "russian")
-         {
-           Console.WriteLine("You've selected Russian\n");
-           difficulty = "Hard"; 
-         
-          }else if (answer == "chinese")
-          {
-          difficulty = "Medium"; 
-           Console.WriteLine("You've selected Chinese\n");
-         }
+     Console.WriteLine(" Would you like to learn Spanish, Russian, Japanese, or Chinese\n");
+     answer = Console.ReadLine().ToLower();
+     if (answer == "spanish")answer.ToLower().StartsWith("s")
 
-         } else if (answer == "no") answer.ToLower().StartsWith("n"); 
-         {
-          Console.WriteLine("No language for you !!\n");
-         }
-         return answer;
-       } 
-
-       static void spanish()
-       {
-         Console.WriteLine("You can learn from duolingo!https://www.duolingo.com/\n");
-      
-       }
-       static void japanese()
-       {
-        Console.WriteLine("You can learn from japanese pod 101! https://www.japanesepod101.com/free-lifetime-account?src=gc_learn_search_brandedsearch_japanese-brand_usa_c__392829551250_&gad_source=1&gclid=EAIaIQobChMIuO\n");        
-       }
-       static void russian()
-       {
-        Console.WriteLine("You can learn from Russian for free!https://www.russianforfree.com/lessons-russian-language-01.php\n");        
-               }
-       static void chinese()
-       {
-        Console.WriteLine("You can learn from Mandarin blueprint!https://www.mandarinblueprint.com/blog/best-way-to-learn-mandarin/\n");        
-        
-       }
-         static void Main(string[] args)  
+      {
+        { 
+         Console.WriteLine(" You've selected Spanish!\n");
+         difficulty = "Easy"; 
+        } else if (answer == "japanese") answer.ToLower().StartsWith("j")
         {
-        
-         
-         int time = 0;
-         bool pay = false;
-         string option = language();      
-         if(option == "spanish")
-         {
-          spanish();
-         } else if (option == "japanese")
-         {
-          japanese();
-         }else if (option == "russian")
-         {
-          russian();
-         }else if (option == "chinese")
-         {
-          chinese();
-         }
-        
+         Console.WriteLine("You've selected Japanese \n"); 
+         difficulty = "Medium";
+        }else if (answer == "russian")answer.ToLower().StartsWith("r")
+        {
+         Console.WriteLine("You've selected Russian\n");
+         difficulty = "Hard"; 
 
-       }  
+        }else if (answer == "chinese")answer.ToLower().StartsWith("c"); 
+        {
+          Console.WriteLine("You've selected Chinese\n");
+          difficulty = "Hard"; 
+        }
+
+      } else if (answer == "no") answer.ToLower().StartsWith("n"); 
+      {
+      Console.WriteLine("No language for you !!\n");
+      }
+    return answer;
+    } 
+
+  static void spanish()
+    {
+      Console.WriteLine("You can learn from duolingo!https://www.duolingo.com/\n");
 
     }
+    static void japanese()
+    {
+    Console.WriteLine("You can learn from japanese pod 101! https://www.japanesepod101.com/free-lifetime-account?src=gc_learn_search_brandedsearch_japanese-brand_usa_c__392829551250_&gad_source=1&gclid=EAIaIQobChMIuO\n");        
+    }
+    static void russian()
+    {
+    Console.WriteLine("You can learn from Russian for free!https://www.russianforfree.com/lessons-russian-language-01.php\n");        
+    }
+    static void chinese()
+    {
+    Console.WriteLine("You can learn from Mandarin blueprint!https://www.mandarinblueprint.com/blog/best-way-to-learn-mandarin/\n");        
+
+    }
+    static void Main(string[] args)  
+    {
+
+
+    int time = 0;
+    bool pay = false;
+    string option = language();      
+    if(option == "spanish")
+      {
+      spanish();
+      } else if (option == "japanese")
+      {
+      japanese();
+      }else if (option == "russian")
+      {
+      russian();
+      }else if (option == "chinese")
+      {
+      chinese();
+      }
+
+
+    }  
+
+  }
 }
 
 // code review by: Ashley
